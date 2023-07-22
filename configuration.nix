5fb2c14a48570bc6d15cb1de232861d1b12a569f
stilxam <m.l.h.litsios@student.tue.nl>
2023-07-22 13:03:06 +0200

    nixConfig

---
+++ configuration.nix
@@ -0,0 +1,242 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "stacy"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Zurich";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_CH.UTF-8";
+    LC_IDENTIFICATION = "fr_CH.UTF-8";
+    LC_MEASUREMENT = "fr_CH.UTF-8";
+    LC_MONETARY = "fr_CH.UTF-8";
+    LC_NAME = "fr_CH.UTF-8";
+    LC_NUMERIC = "fr_CH.UTF-8";
+    LC_PAPER = "fr_CH.UTF-8";
+    LC_TELEPHONE = "fr_CH.UTF-8";
+    LC_TIME = "fr_CH.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ovindar = {
+    isNormalUser = true;
+    description = "Maxwell";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      thunderbird
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs;
+    let
+      r-with-my-pkgs = rWrapper.override{
+        packages = with rPackages;
+	  [
+	  lavaan 
+	  tidyverse
+	];
+      };
+      my-python-pkgs = ps: with ps; [
+      pandas
+      numpy
+      matplotlib
+      seaborn
+      xgboost
+      scipy
+      scikit-learn
+      plotly
+
+      pypdf2
+      nltk
+      langchain
+      
+      pytorch
+      tensorflow
+      ];
+      my-hask-pkgs = haskellPackages.ghcWithPackages(pkgs: with pkgs; [
+      cabal-install
+      haskell-language-server
+      ]);
+
+    in
+  [
+	alacritty
+	neofetch
+	htop
+	wget
+	
+	pciutils
+	gnumake
+	cudatoolkit
+
+	r-with-my-pkgs
+	(python3.withPackages my-python-pkgs)
+	my-hask-pkgs
+#	(import (builtins.fetchTarball {
+	#url = "https://github.com/haskell/ghcup/archive/refs/tags/0.0.7.tar.gz";
+	#sha256 ="17xcl0x4j8n2j9c050ssvdzm9906jcn56b7p2l57z42pqy9d3b43" ;
+#	}) {}) 
+
+
+	nodejs
+	gcc
+	rustup
+	cargo
+
+	gimp
+	obs-studio
+	obsidian	
+	anki-bin
+
+
+	discord
+	teams
+	slack
+
+	
+	git
+	gh
+	lazygit
+
+
+	vscode
+	neovim
+	jetbrains.pycharm-professional
+	bootstrap-studio
+  ];
+  fonts.fonts = with pkgs; [
+	#(nerdfonts.override { fonts = [ "DroidSansMono" ]; } )
+	jetbrains-mono
+    ];
+  
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+
+    # Modesetting is needed for most Wayland compositors
+    modesetting.enable = true;
+
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = false;
+
+    # Enable the nvidia settings menu
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  systemd.services.nvidia-control-devices = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.ExecStart = "${pkgs.linuxPackages.nvidia_x11.bin}/bin/nvidia-smi";
+  };
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+  services.flatpak.enable = true;
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+nix.gc = {
+		automatic = true;
+		dates = "weekly";
+		options = "--delete-older-than 7d";
+	};
+
+}
